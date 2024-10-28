69d73202e07a07056f8f89e6b015e77a1c2c57dc
AnalysticsALok <Apisitsahee@gmail.com>
2024-10-28 15:18:55 +0700

    new auth route start

--- server.js
+++ server.js
@@ -2,11 +2,11 @@ require('dotenv').config()
 const { config } = require("dotenv");
 const express = require("express");
 const app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 3050;
 
 
 
-
+app.use('/auth', authRoute)
 
 app.listen(port, () => {
     console.log(`Example app listening at http://localhost:${port}`);
