5859db4c814991d516a38fa859c8b9c730b172ab
nutchanon-k <5105nutchanon@gmail.com>
2024-10-28 14:13:11 +0700

    init

---
+++ server.js
@@ -0,0 +1,13 @@
+require('dotenv').config()
+const { config } = require("dotenv");
+const express = require("express");
+const app = express();
+const port = process.env.PORT || 3000;
+
+
+
+
+
+app.listen(port, () => {
+    console.log(`Example app listening at http://localhost:${port}`);
+})
