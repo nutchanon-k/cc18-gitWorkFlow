1c96586e13006e534b1b5541f1bad21c05ccf022
Tteenimith <tteenimith@gmail.com>
2024-10-28 15:20:02 +0700

    product test

--- server.js
+++ server.js
@@ -1,11 +1,12 @@
 require('dotenv').config()
 const { config } = require("dotenv");
 const express = require("express");
+const authRoute = require('./routes/authRoute');
 const app = express();
 const port = process.env.PORT || 3000;
 
 
-
+app.use("/auth",authRoute)
 
 
 app.listen(port, () => {
